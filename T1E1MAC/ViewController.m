c95305e6facd323767c4e3e005541867698c8e48
Maria Alonso Carrion <guayser@macbook-pro-de-maria-alonso-carrion.local>
2014-02-11 11:21:48 +0100

    Añadiendo texto para el commit del ejercicio 2

--- T1E1MAC/ViewController.m
+++ T1E1MAC/ViewController.m
@@ -18,7 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"Hello World!!");
+    NSLog(@"Hello World!!, Today is a wonderfull day :-)");
 }
 
 - (void)didReceiveMemoryWarning
